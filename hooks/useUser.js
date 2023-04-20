94fd87f66fd9c5c3b9efd3f8e1cc8a6bad94257d
GaladrosV2 <galadros@gmail.com>
2023-04-20 12:19:39 +0200

    FIX: Tavern

---
+++ hooks/useUser.js
@@ -0,0 +1,29 @@
+import { useState, useEffect } from "react";
+import { useSession } from "next-auth/react";
+import databaseFetch from "../lib/databaseFetch";
+
+const useUser = () => {
+  const [user, setUser] = useState(null);
+  const { data: session } = useSession();
+
+  useEffect(() => {
+    const getUser = async () => {
+      if (session?.user?.email) {
+        const userData = await databaseFetch({
+          action: "findUnique",
+          where: {
+            email: session.user.email,
+          },
+        });
+
+        setUser(userData);
+      }
+    };
+
+    getUser();
+  }, [session]);
+
+  return user;
+};
+
+export default useUser;
